0a76361d4eeded1f1b9dedd788a4ffaf2df07965
Maziar <sojodyan@gmail.com>
2022-06-08 18:21:08 -0400

    Hello, World! app in react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("The app is running")
+import React, { Component} from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+
+  render(){
+    return <div>
+      <h1>Hello, world!</h1>
+    </div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
